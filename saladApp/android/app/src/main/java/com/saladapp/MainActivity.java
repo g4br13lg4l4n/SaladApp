a20462c4fd0d5d5c194a83858cc56afd3d136fe2
Gabriel Galan <gabriel.galan92@gmail.com>
2018-12-24 16:03:43 -0600

    stable

--- saladApp/android/app/src/main/java/com/saladapp/MainActivity.java
+++ saladApp/android/app/src/main/java/com/saladapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.saladapp;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+//import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 //import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends ReactActivity {
@@ -17,12 +17,4 @@ public class MainActivity extends ReactActivity {
         return "saladApp";
     }
 
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-        };
-    }
 }
--- saladApp/android/app/src/main/java/com/saladapp/MainApplication.java
+++ saladApp/android/app/src/main/java/com/saladapp/MainApplication.java
@@ -8,47 +8,10 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
-
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends NavigationApplication {
-    
-    @Override
-    protected ReactGateway createReactGateway() {
-        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
-            @Override
-            protected String getJSMainModuleName() {
-                return "index";
-            }
-        };
-        return new ReactGateway(this, isDebug(), host);
-    }
 
-    @Override
-    public boolean isDebug() {
-        return BuildConfig.DEBUG;
-    }
-
-    protected List<ReactPackage> getPackages() {
-        // Add additional packages you require here
-        // No need to add RnnPackage and MainReactPackage
-        return Arrays.<ReactPackage>asList(
-            // eg. new VectorIconsPackage()
-        );
-    }
-  
-    @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
-        return getPackages();
-    }
-
-}
-
-/*
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -80,4 +43,4 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this,  false);
   }
-}*/
+}
