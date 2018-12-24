28c8a493f1233ae78a9e15f98e9384f3a98a94ec
Gabriel Galan <gabriel.galan92@gmail.com>
2018-12-24 15:58:12 -0600

    after reset

--- saladApp/App.js
+++ saladApp/App.js
@@ -1,18 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {createStackNavigator} from 'react-navigation';
+//import React, {Component} from 'react'
+import {createStackNavigator} from 'react-navigation'
 import Welcome from './Components/Welcome/welcome'
+import Login from './Components/Login/Login'
 
 const App = createStackNavigator({
   Welcome: {screen: Welcome},
-  Profile: {screen: ProfileScreen},
+  Login: {screen: Login},
 });
 
 module.exports = App
