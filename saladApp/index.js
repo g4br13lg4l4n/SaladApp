ebeccda3a52047a872b1878ca98741b82491d47e
Gabriel Galan <gabriel.galan92@gmail.com>
2018-12-24 16:36:54 -0600

    rename

--- saladApp/index.js
+++ saladApp/index.js
@@ -1,10 +1,10 @@
 /** @format */
 
 import {AppRegistry} from 'react-native'
-//import App from './App';
+//import App from './App'
 import {name as appName} from './app.json'
 import Login from './Components/Login/Login'
 import Welcome from './Components/Welcome/welcome'
 
 
-AppRegistry.registerComponent(appName, () => Welcome);
+AppRegistry.registerComponent(appName, () => Login);
