35c8f46da4ef427aedfc728660e7026b26b3e15a
Gabriel Galan <gabriel.galan92@gmail.com>
2018-12-26 16:24:28 -0600

    save stable

--- SaladApp/App.js
+++ SaladApp/App.js
@@ -1,28 +1,49 @@
-import React, {Component} from 'react'
-import { createStackNavigator, createAppContainer } from "react-navigation"
-import { Platform } from 'react-native'
-import Login from './components/Login/Login'
-import Welcome from './components/Welcome/welcome'
-import Signup from './components/SignUp/Signup'
-const ios = Platform.OS === 'ios' 
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-class App extends Component{
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
   render() {
-    return AppNavigator
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
   }
 }
-const AppNavigator = createStackNavigator({
-  Welcome: {
-    screen: Welcome
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  Login: {
-    screen: Login
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  Signup: {
-    screen: Signup
-  }
-})
-
-
-
-module.exports = createAppContainer(AppNavigator)
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- SaladApp/android/app/src/main/java/com/saladapp/MainApplication.java
+++ SaladApp/android/app/src/main/java/com/saladapp/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.auth.RNFirebaseAuthPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage(),
-            new RNGestureHandlerPackage(),
-            new RNFirebaseAuthPackage()
+            new RNFirebasePackage()
       );
     }
 
