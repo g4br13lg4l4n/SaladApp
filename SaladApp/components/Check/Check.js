9b9f33b095f72b2c9026d41811c9a0b6dd37d504
Gabriel Galan <gabriel.galan92@gmail.com>
2019-01-08 22:33:00 -0600

    fix styleGeneral

--- SaladApp/components/Check/Check.js
+++ SaladApp/components/Check/Check.js
@@ -1,4 +1,5 @@
 import { View, Button, Text, TouchableOpacity, TextInput, ScrollView, StyleSheet} from 'react-native'
+import {stylesGeneral} from '../StyleGeneral/styles'
 import React, {Component} from 'react'
 import Styles from './styles'
 
@@ -63,31 +64,5 @@ class Check extends Component {
     )
   }
 }
-
-const stylesGeneral = StyleSheet.create({
-  background: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'white'
-},
-  inputText: {
-    borderColor: '#E0E0E0',
-    borderWidth: 1,
-    width: '80%',
-    fontSize: 16,
-    paddingLeft: 12,
-    marginTop: 10,
-    marginRight: 'auto',
-    marginBottom: 10,
-    marginLeft: 'auto',
-  },
-  buttonPrimary: {
-    width: '80%',
-    marginTop: 20,
-    marginRight: 'auto',
-    marginBottom: 10,
-    marginLeft: 'auto',
-  }
-})
 
 module.exports = Check
