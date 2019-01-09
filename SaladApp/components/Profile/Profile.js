fbac42e0d09bc6649ef718efa71be2a2618a7d9e
Gabriel Galan <gabriel.galan92@gmail.com>
2019-01-08 21:44:11 -0600

    save screen perfil

--- SaladApp/components/Profile/Profile.js
+++ SaladApp/components/Profile/Profile.js
@@ -1,21 +1,51 @@
 import {stylesGeneral} from '../StyleGeneral/styles'
+import firebase from 'react-native-firebase'
 import {View, Text} from 'react-native'
 import React, {Component} from 'react'
 import Styles from './styles'
 
+
 class Profile extends Component {
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      phone: ''
+    }
+  }
+
+  componentDidMount() {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          firebase.database().ref(`users/${user.uid}`).once('value')
+          .then( resp => {
+            this.setState({
+              name: resp.val().name,
+              email: resp.val().email,
+              phone: resp.val().phone
+            })
+          })
+        }
+      })
+  }
+
   render() {
     return (
       <View style={stylesGeneral.background}>    
         <Text style={Styles.title}> PERFIL </Text>  
-        <View>
+        <View style={Styles.margin}>
           <Text style={Styles.text}> Usuario: </Text> 
-          <Text style={Styles.text}> tstets </Text> 
+          <Text style={Styles.text}> {this.state.name} </Text> 
+        </View>
+        <View style={Styles.margin}>
           <Text style={Styles.text}> Correo: </Text> 
-          <Text style={Styles.text}> tstets </Text> 
+          <Text style={Styles.text}> {this.state.email} </Text> 
+        </View>
+        <View style={Styles.margin}>
           <Text style={Styles.text}> Teléfono: </Text> 
-          <Text style={Styles.text}> tstets </Text> 
+          <Text style={Styles.text}> {this.state.phone} </Text> 
         </View>
       </View>
     )
--- SaladApp/components/Profile/styles.js
+++ SaladApp/components/Profile/styles.js
@@ -3,16 +3,18 @@ import { StyleSheet } from 'react-native'
 const Styles = StyleSheet.create({
   title: {
     margin: '10%',
-    fontSize: 60,
+    fontSize: 40,
     color: '#000000',
     letterSpacing: 2.25,
     textAlign: 'center'
   },
   text: {
-    margin: '10%',
-    fontSize: 20,
+    marginLeft: '10%',
+    fontSize: 18,
     color: '#333333',
+  },
+  margin: {
+    marginBottom: 18
   }
 })
-
-module.exports = {Styles}
+module.exports = Styles
