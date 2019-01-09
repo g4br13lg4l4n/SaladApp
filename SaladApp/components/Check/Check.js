faa489f8ae49b47d8ebfd4ce0be688dbdcb1b1f2
Gabriel Galan <gabriel.galan92@gmail.com>
2019-01-08 22:26:38 -0600

    add screen check

--- SaladApp/components/Check/Check.js
+++ SaladApp/components/Check/Check.js
@@ -1,14 +1,93 @@
+import { View, Button, Text, TouchableOpacity, TextInput, ScrollView, StyleSheet} from 'react-native'
 import React, {Component} from 'react'
-import { View, Button, Text, TouchableOpacity} from 'react-native'
+import Styles from './styles'
 
 class Check extends Component {
   render() {
     return (
-      <View>
-        <Text> Screen Check </Text>
-      </View>
+      <ScrollView style={stylesGeneral.background}>    
+        <Text style={Styles.title}> PERFIL </Text>  
+        <Text style={Styles.text}> cantidad a pagar: </Text> 
+        <View style={Styles.margin}>
+          <TextInput 
+            style={stylesGeneral.inputText}
+            onChangeText={(email) => this.setState({email})}
+            placeholder="Cantidad con la que desea pagar"
+          />
+        </View>
+        <View style={Styles.margin}>
+          <TextInput 
+            style={stylesGeneral.inputText}
+            onChangeText={(email) => this.setState({email})}
+            placeholder="Teléfono"
+          />
+        </View>
+        <View style={Styles.margin}>
+          <TextInput 
+            style={stylesGeneral.inputText}
+            onChangeText={(email) => this.setState({email})}
+            placeholder="Estado"
+          />
+        </View>
+        <View style={Styles.margin}>
+          <TextInput 
+            style={stylesGeneral.inputText}
+            onChangeText={(email) => this.setState({email})}
+            placeholder="Colonia"
+          />
+        </View>
+        <View style={Styles.margin}>
+          <TextInput 
+            style={stylesGeneral.inputText}
+            onChangeText={(email) => this.setState({email})}
+            placeholder="Calle"
+          />
+        </View>
+        <View style={Styles.margin}>
+          <TextInput 
+            style={stylesGeneral.inputText}
+            onChangeText={(email) => this.setState({email})}
+            placeholder="Número de casa o departamento"
+          />
+        </View>
+        <View style={Styles.margin}>
+          <TextInput 
+            multiline={true}
+            numberOfLines={4}
+            style={stylesGeneral.inputText}
+            onChangeText={(email) => this.setState({email})}
+            placeholder="Referencia del domicilio "
+          />
+        </View>
+      </ScrollView>
     )
   }
 }
+
+const stylesGeneral = StyleSheet.create({
+  background: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white'
+},
+  inputText: {
+    borderColor: '#E0E0E0',
+    borderWidth: 1,
+    width: '80%',
+    fontSize: 16,
+    paddingLeft: 12,
+    marginTop: 10,
+    marginRight: 'auto',
+    marginBottom: 10,
+    marginLeft: 'auto',
+  },
+  buttonPrimary: {
+    width: '80%',
+    marginTop: 20,
+    marginRight: 'auto',
+    marginBottom: 10,
+    marginLeft: 'auto',
+  }
+})
 
 module.exports = Check
---
+++ SaladApp/components/Check/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+
+const Styles = StyleSheet.create({
+  title: {
+    margin: '10%',
+    marginBottom: '8%',
+    fontSize: 40,
+    color: '#000000',
+    letterSpacing: 2.25,
+    textAlign: 'center'
+  },
+  text: {
+    marginLeft: '10%',
+    fontSize: 20,
+    color: '#333333',
+  },
+  textf: {
+    marginLeft: '10%',
+    fontSize: 16,
+    color: '#333333',
+  },
+  margin: {
+    marginBottom: 18
+  }
+})
+module.exports = Styles
