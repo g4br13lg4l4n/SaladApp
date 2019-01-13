8bbfaf1c9231db7f09d997ee7934b4d9c4cc0543
Gabriel Galan <gabriel.galan92@gmail.com>
2019-01-13 15:20:55 -0600

    stable desing of choose

--- SaladApp/components/Select/Select.js
+++ SaladApp/components/Select/Select.js
@@ -1,8 +1,12 @@
-import {View, Text, TouchableHighlight, Image, FlatList} from 'react-native'
+import {View, Text, TouchableHighlight, Image, FlatList, ScrollView} from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import {stylesGeneral} from '../StyleGeneral/styles'
 import React, {Component} from 'react'
 import Styles from './styles'
+
+const Tomate = require('../../imgs/tomate.png')
+const Lechuga = require('../../imgs/lechuga.png')
+const Aguacate = require('../../imgs/aguacate.png')
 
 class Select extends Component {
   constructor(props) {
@@ -12,17 +16,17 @@ class Select extends Component {
         category: 'VERDURA',
         name: 'Tomate',
         calorias: '50',
-        img: '../../imgs/tomate.png'
+        img: Tomate
       },{
         category: 'VERDURA',
         name: 'Lechuga',
         calorias: '10',
-        img: '../../imgs/lechuga.png'
+        img: Lechuga
       },{
         category: 'VERDURA',
         name: 'Aguacate',
         calorias: '200',
-        img: '../../imgs/aguacate.png'
+        img: Aguacate
       }],
     }
   }
@@ -30,44 +34,57 @@ class Select extends Component {
   render(){
     return(
       <View style={stylesGeneral.backgroundBlue}>
+        <ScrollView style={Styles.bodyScroll}>
+          <View style={Styles.head}>
+            <TouchableHighlight style={Styles.constIcon}>
+              <Icon.Button name="shopping-basket" color="#FF7A27" backgroundColor="transparent"> </Icon.Button>
+            </TouchableHighlight>
 
-        <View style={Styles.head}>
-          <TouchableHighlight style={Styles.constIcon}>
-            <Icon.Button name="shopping-basket" color="#FF7A27" backgroundColor="transparent"> </Icon.Button>
-          </TouchableHighlight>
+            <Text style={Styles.title}>Construye tu ensalada</Text> 
 
-          <Text style={Styles.title}>Construye tu ensalada</Text> 
+            <TouchableHighlight style={Styles.constIconCircle}>
+              <Icon.Button name="shopping-bag" color="#FFFFFF" style={Styles.icon} size={16} backgroundColor="transparent"> </Icon.Button>
+            </TouchableHighlight>
+          </View> 
 
-          <TouchableHighlight style={Styles.constIconCircle}>
-            <Icon.Button name="shopping-bag" color="#FFFFFF" style={Styles.icon} size={16} backgroundColor="transparent"> </Icon.Button>
-          </TouchableHighlight>
-        </View> 
-
-        <View style={Styles.view}>
-          <Image style={Styles.imgSalad}
-              source={require('../../imgs/imgSelect.png')}
-            />
-            <Text style={Styles.titleText}>SELECCIONA LOS INGREDIENTES DE TU ENSALADA</Text> 
-        </View>
-
-        <View style={Styles.viewScroll}>
-
-          <FlatList
-            data={this.state.data}
-            renderItem={({item}) => 
-            <View style={Styles.view}>
-                <Image style={Styles.imgSalad}
-                   source={require(''+item.img+'')}
-                />
-                <Text style={Styles.titleText}>{item.category}</Text> 
-                <Text style={Styles.titleText}>{item.name}</Text> 
-                <Text style={Styles.titleText}>{item.calorias}</Text> 
-            </View>
-          }
-          />
-                
-        </View>
+          <View style={Styles.view}>
+            <Image style={Styles.imgSalad}
+                source={require('../../imgs/imgSelect.png')}
+              />
+              <Text style={Styles.titleText}>SELECCIONA LOS INGREDIENTES DE TU ENSALADA</Text> 
+          </View>
           
+
+          <View style={Styles.viewScroll}>
+            <FlatList
+                style={Styles.flatList}
+                data={this.state.data}
+                renderItem={ ({item}) => 
+              
+              <View style={Styles.view}>
+                  <Image style={Styles.imgSalad}
+                    source={item.img}
+                  />
+                  <Text style={Styles.categoryText}>{item.category}</Text> 
+                  <Text style={Styles.frutName}> {item.name}</Text> 
+
+                  <View style={Styles.contentInfo}>
+                    <View>
+                      <Icon.Button name="tasks" color="#BDBDBD" backgroundColor="transparent">{item.calorias}</Icon.Button>
+                    </View>
+                    <TouchableHighlight> 
+                      <Icon.Button name="heart" color="#BDBDBD" backgroundColor="transparent">Agregar</Icon.Button> 
+                    </TouchableHighlight>
+                  </View>
+
+              </View>
+            }
+            keyExtractor={(item, index) => index.toString()}
+            />  
+          </View>
+
+
+        </ScrollView>
       </View>
     )
   }
--- SaladApp/components/Select/styles.js
+++ SaladApp/components/Select/styles.js
@@ -1,6 +1,11 @@
 import { StyleSheet } from 'react-native'
 
 const Styles = StyleSheet.create({
+  bodyScroll: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    flexDirection: 'column',
+  },
   head:{
     justifyContent:'space-between',
     flexDirection:'row',
@@ -8,22 +13,19 @@ const Styles = StyleSheet.create({
     marginRight: '5%',
     marginTop: '4%',
     marginBottom: 5,
+    height: 35,
   },
   view: {
     marginLeft: '5%',
     marginRight: '5%',
     marginTop: '4%',
     marginBottom: '4%',
-  },
-  viewScroll:{
-    marginTop: '4%',
-    marginBottom: 20,
-    paddingTop: 4,
-    paddingBottom: 10,
+    backgroundColor: 'white'
   },
   title: {
     paddingTop: 6,
     fontSize: 16,
+    height: 32,
     color: '#333333',
     letterSpacing: 2.25,
     textAlign: 'center',
@@ -54,9 +56,39 @@ const Styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'center',
   },
-  container: {
-    flex: 1,
-    marginTop: 20,
+  viewScroll: {
+    height: 800,
+    marginTop: '4%',
+    marginBottom: 20,
+    paddingTop: 4,
+    paddingBottom: 10,
+  },
+  flatList: {
+
+  },
+  categoryText: {
+    marginTop: 10,
+    color: '#9E9E9E',
+    letterSpacing: 1,
+    fontSize: 12,
+    textAlign: 'left',
+    marginLeft: '5%',
+    marginRight: '5%',
+  },
+  frutName: {
+    marginTop: 10,
+    color: '#333333',
+    fontSize: 18,
+    lineHeight: 20,
+    letterSpacing: 0.5,
+    textAlign: 'left',
+    marginLeft: '5%',
+    marginRight: '5%',
+  },
+  contentInfo:{
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    backgroundColor: 'white',
   }
 })
 module.exports = Styles
