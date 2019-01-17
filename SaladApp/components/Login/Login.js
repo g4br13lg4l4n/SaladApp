6ce83a18b9f58ae0a7e9bf8276d0d1080bfa6714
Gabriel Galan <gabriel.galan92@gmail.com>
2019-01-16 19:47:52 -0600

    add message error and loadin

--- SaladApp/components/Login/Login.js
+++ SaladApp/components/Login/Login.js
@@ -1,4 +1,5 @@
-import {View, Button, TextInput, Text, TouchableOpacity, Platform} from 'react-native'
+import {View, TextInput, Text, TouchableOpacity, Platform, ActivityIndicator} from 'react-native'
+import AwesomeAlert from 'react-native-awesome-alerts'
 import {stylesGeneral} from '../StyleGeneral/styles'
 import firebase from 'react-native-firebase'
 import React, {Component} from 'react'
@@ -11,24 +12,53 @@ class Login extends Component {
     super(props);
     this.state = {
       email: '',
-      password: ''
-    };
+      password: '',
+      showAlert: false,
+      loading: false
+    }
+    this.showAlert = this.showAlert.bind(this)
+    this.hideAlert = this.hideAlert.bind(this)
+    this.showLoading = this.showLoading.bind(this)
+  }
+
+  showLoading() {
+    if(this.state.loading){
+      return (<View><ActivityIndicator size="small" color="#FFFFFF" /></View>)
+    }else {
+      return (<Text style={stylesGeneral.textButton}> ENTRAR </Text>)
+    }
+  }
+
+  showAlert() {
+    this.setState({
+      showAlert: true
+    })
+  }
+
+  hideAlert() {
+    this.setState({
+      showAlert: false,
+      loading: false
+    })
   }
 
   login() {
     if(!this.state.email == '' && !this.state.password == ''){
+      this.setState({
+        loading: true
+      })
       firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
       .then(res => {
-        //console.warn('respuesta ---->', res)
         this.props.navigation.push('TabNavigation')
       })
       .catch(err => {
-        //console.warn('error --->', err)
+        this.showAlert()
       })
     }
   }
 
   render() {
+    let loading = this.showLoading()
     return (
       <View style={stylesGeneral.background}>
         <Text style={Styles.title}>
@@ -51,8 +81,22 @@ class Login extends Component {
         <TouchableOpacity 
           onPress={()=> { this.login() }}
           style={stylesGeneral.button}>
-          <Text style={stylesGeneral.textButton}> ENTRAR </Text>
+          {loading}
         </TouchableOpacity>
+
+        <AwesomeAlert
+          show={this.state.showAlert}
+          showProgress={false}
+          message="Su usuario o contraseña son incorrectos"
+          closeOnTouchOutside={false}
+          closeOnHardwareBackPress={false}
+          showConfirmButton={true}
+          confirmText="Cerrar"
+          confirmButtonColor="#4093f1"
+          onConfirmPressed={() => {
+            this.hideAlert()
+          }}
+        />
       </View>
     )
   }
